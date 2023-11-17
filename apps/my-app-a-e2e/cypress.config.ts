32711658ca26e32e167ca358156c2581fcee72f1
Adrian Baran <adbaran@cisco.com>
2023-11-17 15:07:22 -0600

    chore: reproduce Webpack Compilation Error with Cypress 13

--- apps/my-app-a-e2e/cypress.config.ts
+++ apps/my-app-a-e2e/cypress.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { getPreprocessorConfig } from '@jscutlery/cypress-harness/preprocessor-config';
 
 export default defineConfig({
   e2e: nxE2EPreset(__dirname),
+  ...getPreprocessorConfig
 });
